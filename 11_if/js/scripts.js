6966b6f03fb141dbd80a84402bdcb84446695b72
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-07 01:44:53 -0300

    condições if aula 11

---
+++ 11_if/js/scripts.js
@@ -0,0 +1,11 @@
+var idade = 16;
+var idadeMinima = 18;
+
+
+if(idade > idadeMinima) {
+    console.log("Pode fazer a carteira de habilitação");
+}
+
+if(idade > 18) {
+    console.log("Precisa esperar 3 anos ainda");
+}
---
+++ 12_op_compração/js/scripts.js
@@ -0,0 +1,19 @@
+var idade = 18;
+
+if(idade >= 18) {
+    console.log("O usuário pode fazer carteira de habilitação");
+}
+
+if(idade <= 17) {
+    console.log("O usuário não pode fazer a carteira")
+}
+
+var nome = "Matheus";
+
+if(nome == "Felipe") {
+    console.log('voce é o Felipe')
+}
+
+if(nome != "Felipe") {
+    console.log("O nome não é Felipe")
+}
