ab6e34a8a816398fc5b68c6feb2a5b59ec678192
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-07 00:16:09 -0300

    string

---
+++ 6_string/js/scripts.js
@@ -0,0 +1,21 @@
+var nome = 'matheus';
+var sobrenome = 'battisti';
+
+
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "querty");
+console.log(nomeCompleto);
+
+console.log(typeof "5.292929")
+
+var frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('ele disse "olá"');
