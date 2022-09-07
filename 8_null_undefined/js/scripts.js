bce76477d0132e6ba4a5b46b2a87d365adaede44
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-07 01:01:57 -0300

    Hoisting - içamento Null e undefined

---
+++ 8_null_undefined/js/scripts.js
@@ -0,0 +1,13 @@
+// Hoisting - içamento
+
+console.log(sobrenome);
+
+var nome = null;
+var sobrenome = "felipe"
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "felipe";
+
+console.log(nome);
