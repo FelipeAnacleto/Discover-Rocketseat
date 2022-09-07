5cdd7af0fbe2039130ea4a4b58eb0f5d57df6f2e
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-07 12:42:27 -0300

    Operador lógico AND (&&)

---
+++ 15_operador_logico_and/js/scripts.js
@@ -0,0 +1,8 @@
+var idade = 16;
+var nome = "Pedro";
+
+if(nome == "João" && idade == 16) {
+    console.log("O João pode entrar na aula de esgrima");
+} else {
+    console.log("Este não é o João");
+}
