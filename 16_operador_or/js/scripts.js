351e7a287eca1481c6569b9a70dc15bb91b82b9b
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-07 12:50:21 -0300

    Operador lógico OR ||

---
+++ 16_operador_or/js/scripts.js
@@ -0,0 +1,12 @@
+var idade = 12;
+var nome = "Pedro";
+
+if(nome == "Pedro" || idade > 14) {
+    console.log("Pode entrar na aula de esgrima");
+} else {
+    console.log("Não pode entrar")
+}
+
+if(nome == "Pedro" && 15 > 20 || 10 == 10) {
+    console.log("Testando")
+}
