c100f417598b1eac37cf4e118f601d46e561544e
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-07 12:22:47 -0300

    Else if e else

---
+++ Else_if_e_else/js/scripts.js
@@ -0,0 +1,17 @@
+var nome = "Matheus";
+
+if(nome == "pedro") {
+    console.log("O nome dele é Pedro");
+} else if(nome == "Matheus") {
+    console.log("O nome é Matheus")
+} else {
+    console.log("Ele possui outro nome!");
+}
+
+var idade = 22;
+
+if(idade > 20) {
+    console.log("Ele pode entrar na festa");
+} else if(idade >=18) {
+    console.log("Ele so pode entrar com autorização");
+}
