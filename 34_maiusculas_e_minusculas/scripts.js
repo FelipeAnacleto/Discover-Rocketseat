8db92eae07fb6b35fed201116635bf49f26ac227
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-08 01:34:59 -0300

    Transforme letras minusculas em maiúsculas

---
+++ 34_maiusculas_e_minusculas/scripts.js
@@ -0,0 +1,5 @@
+// Transforme letras minusculas em maiúsculas. Faça o contrario disso também
+
+let world = "Programar é muito bacana!"
+console.log(world.toUpperCase());
+console.log(world.toLowerCase());
