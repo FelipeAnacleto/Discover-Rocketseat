17c8cc5dd00ea45101f3df40c2510a88da6c1cc4
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-08 18:39:44 -0300

    Precedência dos operadores

---
+++ 51_Precedência_dos_operadores/scripts.js
@@ -0,0 +1,16 @@
+/**  Operator precedence
+    precedência de operadores
+
+ * grouping                     ( )
+ * negação e incremento          ! ++ --
+ * multiplicação                 * /
+ * adição e subtração            + -
+ * relacional                    < <= > >=c
+ * igualdade                     == != === !== 
+ * AND                           &&
+ * OR                            ||
+ * condicional                   ??
+ * assignment (atribuição)       = += -= *=
+*/
+
+console.log(3 > 2 && 2 > 1);
