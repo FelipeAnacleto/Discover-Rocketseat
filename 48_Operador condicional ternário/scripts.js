cf33bb8f0322384bd338c13d5c688243a9fdb547
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-08 16:51:04 -0300

    Operador condicional ternário

---
+++ 48_Operador condicional ternário/scripts.js
@@ -0,0 +1,13 @@
+// Operador Condicional (Ternário)
+
+// Dependendo da condição, nós receberemos valores diferentes
+
+// Condição então valor 1 se não valor 2
+// condition ? value1: value2
+
+// Exemplos
+
+// Café de manhã top
+
+
+// Maior de 18
