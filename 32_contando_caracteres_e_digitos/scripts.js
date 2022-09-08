39b7f9457edb29e1a113f7b0b79cc1fb36f15222
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-08 01:24:16 -0300

    Manipulando Strings e Números

---
+++ 32_contando_caracteres_e_digitos/scripts.js
@@ -0,0 +1,8 @@
+// Manipulando Strings e Números
+
+// Contar quantos caracteres tem uma palavra e quantos digitos um número
+
+let world = 'Paralelepipedo'
+console.log(world.length);
+let number = 1234;
+console.log(String(number).length)
