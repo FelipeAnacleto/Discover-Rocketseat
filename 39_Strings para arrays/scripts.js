4d45db7f421f2f644106bd3f9c41cad2c735f8b8
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-08 09:40:32 -0300

    Manipulando dados

--- 39_Strings para arrays/scripts.js
+++ 39_Strings para arrays/scripts.js
@@ -1,4 +1,28 @@
-// Transformando elementos de caracteres em elementos de um array
+    // Manipulando Arrays
 
-let world = "manipulação"
-console.log(Array.from(world))
+    let techs = ["html", "css", "js"]
+
+    // adicionar um item no fim
+    techs.push("nodejs");
+
+    // adicionar um item no começo
+    techs.unshift("sql")
+
+    // remover do fim
+    techs.pop()
+
+    // remover do começo
+    techs.shift()
+
+    // pegar somente alguns elementos do array
+    console.log(techs.slice(1, 3))
+
+    // remover 1 ou mais items em qualquer posição do array
+    techs.splice(1, 3)
+
+    // encontrar a posição de um elemento no array
+    let index = techs.indexOf('sql')
+    techs.splice(index, 1)
+
+
+    console.log(techs)
